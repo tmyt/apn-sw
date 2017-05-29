6412a7b76474c4b5bdb0499d581b999115f97384
Nobuhiro Ito <iseebi@iseteki.net>
2017-05-29 23:50:28 +0900

    編集画面のアウトレット接続

--- MvnoSwitcher/MSEditPageViewController.cs
+++ MvnoSwitcher/MSEditPageViewController.cs
@@ -14,6 +14,32 @@ namespace MvnoSwitcher
         public ConfigGenerator Config { get; internal set; }
         public int Index { get; internal set; }
 
-        
+        public override void ViewWillAppear(bool animated)
+        {
+            base.ViewWillAppear(animated);
+            NameTextField.Text = Config.Name;
+            ApnTextField.Text = Config.Apn;
+            UsernameTextField.Text = Config.Username;
+            PasswordText.Text = Config.Password;
+        }
+
+        partial void SaveTapped(Foundation.NSObject sender)
+        {
+            Config.Name = NameTextField.Text;
+            Config.Apn = ApnTextField.Text;
+            Config.Username = UsernameTextField.Text;
+            Config.Password = PasswordText.Text;
+
+            int authIndex = (int)AuthenticationTypeSegmentedControl.SelectedSegment;
+            Config.AuthenticationType = (authIndex == 1) ? "CHAP" : "PAP";
+
+
+            var appConfig = (UIApplication.SharedApplication.Delegate as AppDelegate).AppConfig;
+            if (IsNew)
+            {
+                appConfig.Apns.Add(Config);
+            }
+            appConfig.Save();
+        }
     }
 }
--- MvnoSwitcher/MSEditPageViewController.designer.cs
+++ MvnoSwitcher/MSEditPageViewController.designer.cs
@@ -1,21 +1,61 @@
-﻿// WARNING
+// WARNING
 //
-// This file has been generated automatically by Visual Studio from the outlets and
-// actions declared in your storyboard file.
-// Manual changes to this file will not be maintained.
+// This file has been generated automatically by Xamarin Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
 //
 using Foundation;
-using System;
 using System.CodeDom.Compiler;
-using UIKit;
 
 namespace MvnoSwitcher
 {
-    [Register ("MSEditPageViewController")]
-    partial class MSEditPageViewController
-    {
-        void ReleaseDesignerOutlets ()
-        {
-        }
-    }
+	[Register ("MSEditPageViewController")]
+	partial class MSEditPageViewController
+	{
+		[Outlet]
+		UIKit.UITextField ApnTextField { get; set; }
+
+		[Outlet]
+		UIKit.UISegmentedControl AuthenticationTypeSegmentedControl { get; set; }
+
+		[Outlet]
+		UIKit.UITextField NameTextField { get; set; }
+
+		[Outlet]
+		UIKit.UITextField PasswordText { get; set; }
+
+		[Outlet]
+		UIKit.UITextField UsernameTextField { get; set; }
+
+		[Action ("SaveTapped:")]
+		partial void SaveTapped (Foundation.NSObject sender);
+		
+		void ReleaseDesignerOutlets ()
+		{
+			if (ApnTextField != null) {
+				ApnTextField.Dispose ();
+				ApnTextField = null;
+			}
+
+			if (NameTextField != null) {
+				NameTextField.Dispose ();
+				NameTextField = null;
+			}
+
+			if (PasswordText != null) {
+				PasswordText.Dispose ();
+				PasswordText = null;
+			}
+
+			if (UsernameTextField != null) {
+				UsernameTextField.Dispose ();
+				UsernameTextField = null;
+			}
+
+			if (AuthenticationTypeSegmentedControl != null) {
+				AuthenticationTypeSegmentedControl.Dispose ();
+				AuthenticationTypeSegmentedControl = null;
+			}
+		}
+	}
 }
