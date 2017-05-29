db7d25f0e405f2444ea40c734e11cbb98f316ce7
yutaka <shigure@refy.net>
2017-05-30 00:19:17 +0900

    fix problem after merge

--- MvnoSwitcher/MSEditPageViewController.designer.cs
+++ MvnoSwitcher/MSEditPageViewController.designer.cs
@@ -1,61 +1,67 @@
-// WARNING
+﻿// WARNING
 //
-// This file has been generated automatically by Xamarin Studio to store outlets and
-// actions made in the UI designer. If it is removed, they will be lost.
-// Manual changes to this file may not be handled correctly.
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
 //
 using Foundation;
+using System;
 using System.CodeDom.Compiler;
 
 namespace MvnoSwitcher
 {
-	[Register ("MSEditPageViewController")]
-	partial class MSEditPageViewController
-	{
-		[Outlet]
-		UIKit.UITextField ApnTextField { get; set; }
+    [Register ("MSEditPageViewController")]
+    partial class MSEditPageViewController
+    {
+        [Outlet]
+        UIKit.UITextField ApnTextField { get; set; }
 
-		[Outlet]
-		UIKit.UISegmentedControl AuthenticationTypeSegmentedControl { get; set; }
 
-		[Outlet]
-		UIKit.UITextField NameTextField { get; set; }
+        [Outlet]
+        UIKit.UISegmentedControl AuthenticationTypeSegmentedControl { get; set; }
 
-		[Outlet]
-		UIKit.UITextField PasswordText { get; set; }
 
-		[Outlet]
-		UIKit.UITextField UsernameTextField { get; set; }
+        [Outlet]
+        UIKit.UITextField NameTextField { get; set; }
 
-		[Action ("SaveTapped:")]
-		partial void SaveTapped (Foundation.NSObject sender);
-		
-		void ReleaseDesignerOutlets ()
-		{
-			if (ApnTextField != null) {
-				ApnTextField.Dispose ();
-				ApnTextField = null;
-			}
 
-			if (NameTextField != null) {
-				NameTextField.Dispose ();
-				NameTextField = null;
-			}
+        [Outlet]
+        UIKit.UITextField PasswordText { get; set; }
 
-			if (PasswordText != null) {
-				PasswordText.Dispose ();
-				PasswordText = null;
-			}
 
-			if (UsernameTextField != null) {
-				UsernameTextField.Dispose ();
-				UsernameTextField = null;
-			}
+        [Outlet]
+        UIKit.UITextField UsernameTextField { get; set; }
 
-			if (AuthenticationTypeSegmentedControl != null) {
-				AuthenticationTypeSegmentedControl.Dispose ();
-				AuthenticationTypeSegmentedControl = null;
-			}
-		}
-	}
+
+        [Action ("SaveTapped:")]
+        partial void SaveTapped (Foundation.NSObject sender);
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (ApnTextField != null) {
+                ApnTextField.Dispose ();
+                ApnTextField = null;
+            }
+
+            if (AuthenticationTypeSegmentedControl != null) {
+                AuthenticationTypeSegmentedControl.Dispose ();
+                AuthenticationTypeSegmentedControl = null;
+            }
+
+            if (NameTextField != null) {
+                NameTextField.Dispose ();
+                NameTextField = null;
+            }
+
+            if (PasswordText != null) {
+                PasswordText.Dispose ();
+                PasswordText = null;
+            }
+
+            if (UsernameTextField != null) {
+                UsernameTextField.Dispose ();
+                UsernameTextField = null;
+            }
+        }
+    }
 }
